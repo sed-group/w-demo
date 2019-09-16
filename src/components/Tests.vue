d30b6a2571d59725a09e8628dff585efc73a094f
Iñigo Alonso Fernandez <inigoa@net.chalmers.se>
2019-09-16 13:43:23 +0200

    added babylonjs

--- src/components/Tests.vue
+++ src/components/Tests.vue
@@ -51,6 +51,21 @@
                   </v-list-item>
                 </v-list-item-group>
               </v-list>
+              <Scene>
+                <Camera></Camera>
+                <HemisphericLight diffuse="#0000FF"></HemisphericLight>
+                <Entity :position="[0, 0, 5]">
+                  <Animation property="rotation.x" :duration="5" :end="Math.PI * 2"></Animation>
+                  <Animation property="rotation.y" :duration="5" :end="Math.PI * 2"></Animation>
+                  <Animation property="rotation.z" :duration="5" :end="Math.PI * 2"></Animation>
+                  <PointLight diffuse="#FF0000"></PointLight>
+                  <template v-for="x in [0, 4, -4]">
+                    <template v-for="y in [0, 4, -4]">
+                      <Box v-for="z in [0, 4, -4]" :position="[x, y, z]" :key="`${x},${y},${z}`"></Box>
+                    </template>
+                  </template>
+                </Entity>
+              </Scene>
             </v-card-text>
           </v-card>
 
@@ -342,11 +357,15 @@
 import draggable from 'vuedraggable'
 //import OtherChart from './OtherChart'
 import { db } from '@/main'
+import { Entity, Scene, Box } from 'vue-babylonjs';
 
   export default {
+    mixins: [Entity],
     components: {
       draggable,
       //OtherChart,
+      Scene,
+      Box,
     },
     computed: {
       weight: function () {
