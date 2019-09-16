f3a9730ff5690d7defe3bf5a7fde1129db89bc7d
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2019-09-16 09:03:10 +0200

    added Vue config

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = { 
+chainWebpack: config => { 
+// disable cache for prod only, remove the if to disable it everywhere 
+// if (process.env.NODE_ENV === 'production') { 
+config.module.rule('vue').uses.delete('cache-loader'); config.module.rule('js').uses.delete('cache-loader'); config.module.rule('ts').uses.delete('cache-loader'); config.module.rule('tsx').uses.delete('cache-loader'); 
+// } },
