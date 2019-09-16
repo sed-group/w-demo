3d8002b8eec394567e999f6b3b48cbaee7316f29
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2019-09-16 09:04:58 +0200

    fixed typo

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,9 @@ module.exports = {
 chainWebpack: config => { 
 // disable cache for prod only, remove the if to disable it everywhere 
 // if (process.env.NODE_ENV === 'production') { 
-config.module.rule('vue').uses.delete('cache-loader'); config.module.rule('js').uses.delete('cache-loader'); config.module.rule('ts').uses.delete('cache-loader'); config.module.rule('tsx').uses.delete('cache-loader'); 
-// } },
+config.module.rule('vue').uses.delete('cache-loader');
+config.module.rule('js').uses.delete('cache-loader');
+config.module.rule('ts').uses.delete('cache-loader');
+config.module.rule('tsx').uses.delete('cache-loader'); 
+// } 
+},
