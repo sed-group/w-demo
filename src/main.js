bf6f97c9ec5126c6b9323ade86d3c1de315c326b
Iñigo Alonso Fernandez <inigoa@net.chalmers.se>
2019-09-20 08:53:21 +0200

    fixed firebase import

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 Vue.config.productionTip = false
 
